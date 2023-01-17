c875d99dd5babaec7312bd0ded8c900c6bc55e91
Frillion <fridriksoebech@gmail.com>
2023-01-17 17:15:40 +0000

    controller testing

--- Scripts/TskoliCompClawBot/src/main.cpp
+++ Scripts/TskoliCompClawBot/src/main.cpp
@@ -18,6 +18,7 @@ int32_t TOP_RIGHT_PORT = 1;
 int32_t TOP_LEFT_PORT = 11;
 int32_t BOTTOM_RIGHT_PORT = 10;
 int32_t BOTTOM_LEFT_PORT = 20;
+int32_t controller_test = 0;
 controller main_controller = controller(primary);
 motor top_left_wheel = motor(TOP_LEFT_PORT);
 motor top_right_wheel = motor(TOP_RIGHT_PORT);
@@ -26,23 +27,11 @@ motor bottom_left_wheel = motor(BOTTOM_LEFT_PORT);
 motor_group left_wheels = motor_group(top_left_wheel,bottom_left_wheel);
 motor_group right_wheels = motor_group(top_right_wheel,bottom_right_wheel);
 drivetrain chassis(left_wheels,right_wheels,319.024,250,130,distanceUnits::mm,1);
-int displayTask(){
-    while(1){
-        Brain.Screen.setCursor(2,1);
-        Brain.Screen.print("Left Speed %4.0f",top_left_wheel.velocity(percent));
-        Brain.Screen.newLine();
-        Brain.Screen.print("Right Speed %4.0f",top_right_wheel.velocity(percent));
-        Brain.Screen.newLine();
-        if(main_controller.installed()){
-            int32_t stick_value = main_controller.Axis1.value();
-            Brain.Screen.print("Controller Axi Value %4.0f",stick_value);
-        }
-        wait(20, timeUnits::msec);
-    }
-    return 0;
+void handleAPress(){
+    main_controller.rumble(".");
 }
 int main() {
-    task displayInstance(displayTask);
+    main_controller.ButtonA.pressed( handleAPress );
     while(1) {
         // Allow other tasks to run
         this_thread::sleep_for(10);
